468faed3afbc43b147bdc9586145dd238ca491e0
Turyam <srikin@outlook.com>
2021-03-31 11:28:23 +0530

    Modified file for Basic Structure Program 2

--- p1.c
+++ p1.c
@@ -13,7 +13,16 @@ int main()
 	printf("Add id and price of the books to our server.");
 	scanf("%d %d", &b.id, &b.price);
 	
+	
+	printf("Add name of the Second books to our server.");
+	scanf("%s",&v.name);
+	printf("Add id and price of the Second books to our server.");
+	scanf("%d %d", &v.id, &v.price);
+	
+	printf("\n##--------------#\n");
 	printf("Name of the book YOu have added is: %s and it's id and price are %d and %d rs.\n",b.name,b.id,b.price);
+	printf("\n##--------------#\n");
+	printf("Name of the book YOu have added is: %s and it's id and price are %d and %d rs.\n",v.name,v.id,v.price);
 	
 
 }
